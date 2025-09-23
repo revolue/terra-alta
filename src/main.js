84b7850260d2c99caa1a0ffca694249cd17bc686
revolue <revolue@github.com>
2025-09-23 15:04:35 -0300

    Add home page design with logo and mission text

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <header>
-      <h1>TERRA ALTA</h1>
-    </header>
-    <main>
-      <p>Site em construção</p>
-    </main>
+    <div class="logo-container">
+      <img src="/ta_final_black.png" alt="TERRA ALTA" class="logo">
+    </div>
+    <div class="text-content">
+      <p>Explorar pequenos sítios, escutar histórias reais, e viver a rotina de quem cultiva o café com dedicação, geração após geração.</p>
+      <p>Nossa missão é entregar a essência do que exploramos de forma autêntica, através da visão de um especialista em café especiais, um artista plástico e um fotógrafo.</p>
+    </div>
   </div>
 `
